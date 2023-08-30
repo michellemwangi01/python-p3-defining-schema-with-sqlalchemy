a50a05f1c38ccdae82a828259d1038b9fbff86b2
michellemwangi01 <michellemwangim@gmail.com>
2023-08-30 14:27:30 +0300

    completed

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+#   Defining a persisiting a simple schema (db) using sqlAlchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine  # create_engine: This is a function provided by SQLAlchemy that creates a database engine. The database engine acts as the interface between your Python code and the actual database. It manages the connections and communication with the database.
 
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    __tablename__ = 'students'  # give the table a name
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 if __name__ == '__main__':
-    pass
+    engine = create_engine('sqlite:///students.db')  # sqlite:/// is just a prefix just indicates that the db about to be mentioned is an sqlite database
+    Base.metadata.create_all(engine)
